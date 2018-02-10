69eb2b08b3ab0778f710e9ae20eaf25d0fb5d719
BTWitsK <BTWitsK@Gmail.com>
2018-02-10 17:21:47 -0600

    skeleton of a pair of dice

--- Dice.py
+++ Dice.py
@@ -1,2 +1,16 @@
-#TODO
-'''Basic Skeleton of a Dice class to be used for games'''
+"""Basic Skeleton of a Dice class to be used for games"""
+
+
+class PairOfDice:
+    #TODO implement both die
+
+    def roll(self):
+        """ Rolls the pair of dice"""
+        #TODO implement roll function
+
+    def value(self):
+        """
+
+        """
+        #Returns the value of the pair of dice
+        #TODO implement value function
